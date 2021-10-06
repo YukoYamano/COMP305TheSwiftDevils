6b92002bdd2d045f6f0b9f9e95b8ca2963ccfaf6
Nandini <juicyfruit5600@yahoo.ca>
2021-10-05 20:05:17 -0400

    LOLJ-37 Created Door Function

---
+++ Assets/Nandini/Scripts/Door/Door_Behaviour.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Door_Behaviour : MonoBehaviour
+{
+    public GameObject prompt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prompt.GetComponent<MeshRenderer>().enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            prompt.GetComponent<MeshRenderer>().enabled = true;
+
+        }
+        if(other.CompareTag("Player") && Input.GetAxis("Fire1") > 0)
+        {
+            SceneManager.LoadScene("Water Puzzle");
+        }
+    }
+}
