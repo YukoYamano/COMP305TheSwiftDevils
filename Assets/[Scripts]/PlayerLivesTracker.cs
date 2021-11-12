7e5e4c11a0c8f672320323e8def91b7a468ed964
Charlie <charlie_bi@hotmail.com>
2021-11-11 21:42:26 -0500

    LOLJ 51 + LOLJ 52 Lives and UI Implemented

---
+++ Assets/[Scripts]/PlayerLivesTracker.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLivesTracker : MonoBehaviour
+{
+    public GameObject[] hearts = new GameObject[5];
+
+    public int lives = 3;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        UpdateLivesUI();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //UpdateLivesUI();
+        if (lives <= 0)
+        {
+            HandleGameOver();
+        }
+    }
+
+    public void DecreaseLives(int amount = 1)
+    {
+        lives -= amount;
+        UpdateLivesUI();
+        if(lives <= 0)
+        {
+            HandleGameOver();
+        }
+    }
+
+    public void IncreaseLives(int amount = 1)
+    {
+        lives += amount;
+        UpdateLivesUI();
+    }
+
+    private void UpdateLivesUI()
+    {
+        for(int i = 0; i < hearts.Length; i++)
+        {
+            if(i < lives)
+            {
+                hearts[i].SetActive(true);
+            }
+            else
+            {
+                hearts[i].SetActive(false);
+            }
+        }
+    }
+
+    private void HandleGameOver()
+    {
+        //load game over scene
+        SceneManager.LoadScene("GameOver");
+    }
+}
