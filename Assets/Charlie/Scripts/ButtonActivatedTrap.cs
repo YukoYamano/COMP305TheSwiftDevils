4557e2ad9cd5c2084dcf758a82ff11a25467ba0d
Charlie <charlie_bi@hotmail.com>
2021-11-18 10:33:54 -0500

    Added a new button type to the prefabs

--- Assets/Charlie/Scripts/ButtonActivatedTrap.cs
+++ Assets/Charlie/Scripts/ButtonActivatedTrap.cs
@@ -2,38 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PressureButton : MonoBehaviour
+public class ButtonActivatedTrap : PressureButton
 {
+
     /// <summary>
     /// This button will only activate with a balloon or stone
     /// The button remains pressed until the weight is removed
+    /// In addition to removing the barrier, it will also activate a trap
     /// </summary>
     /// 
 
-    [Tooltip("Image of inactive button")]
-    [SerializeField] public Sprite unpressed;
-    [Tooltip("Image of active button")]
-    [SerializeField] public Sprite pressed;
-    [Tooltip("The GameObject to be destoyed once button is activated")]
-    [SerializeField] public GameObject destructableObj;
-
-
-    protected SpriteRenderer activeImg;
+    [SerializeField] public GameObject trap;
 
     // Start is called before the first frame update
     void Start()
     {
         activeImg = GetComponent<SpriteRenderer>();
         activeImg.sprite = unpressed;
+        trap.SetActive(false);
+        destructableObj.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+
     }
 
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Balloon") || collision.gameObject.CompareTag("Stone"))
         {
@@ -41,10 +37,11 @@ public class PressureButton : MonoBehaviour
             activeImg.sprite = pressed;
             //remove blocks
             destructableObj.SetActive(false);
+            trap.SetActive(true);
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Balloon") || collision.gameObject.CompareTag("Stone"))
         {
@@ -52,7 +49,8 @@ public class PressureButton : MonoBehaviour
             activeImg.sprite = unpressed;
             //remove blocks
             destructableObj.SetActive(true);
+            trap.SetActive(false);
         }
-    }
 
+    }
 }
