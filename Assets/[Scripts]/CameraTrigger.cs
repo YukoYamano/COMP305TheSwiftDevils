1cf576b952208a0158502046d5be9a0a4b23f576
Tatsiana <tptushko@my.centennialcollege.ca>
2021-12-06 14:28:46 -0500

    Camera zoom fixed

--- Assets/[Scripts]/CameraTrigger.cs
+++ Assets/[Scripts]/CameraTrigger.cs
@@ -6,17 +6,37 @@ public class CameraTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject cameraToActivate;
     [SerializeField] private GameObject cameraOut;
-
+    [SerializeField] private float zoomTime=2.5f;
+    private float shortZoomTime, userZoomTime;
     public MyVirtualCameraController vCamController;
 
+    void Start()
+    {
+        shortZoomTime = 2.5f;
+        userZoomTime = zoomTime;
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            zoomTime = shortZoomTime;
+            Debug.Log("zoom time: " + zoomTime);
             vCamController.TransitionTo(cameraToActivate);
             StartCoroutine(Delay());
         }
     }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && Input.GetKey("e"))
+        {
+            zoomTime = userZoomTime;
+            Debug.Log("zoom time: " + zoomTime);
+            vCamController.TransitionTo(cameraToActivate);
+            StartCoroutine(Delay());
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -28,7 +48,7 @@ public class CameraTrigger : MonoBehaviour
 
     IEnumerator Delay()
     {
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(zoomTime);
         vCamController.TransitionTo(cameraOut);
 
     }
