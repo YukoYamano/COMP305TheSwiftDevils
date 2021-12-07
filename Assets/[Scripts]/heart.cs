b22a35dfb453cfd5a1e719a0a520c79b7b2e0cac
Charlie <charlie_bi@hotmail.com>
2021-12-06 21:38:32 -0500

    added heart fx to heart

--- Assets/[Scripts]/heart.cs
+++ Assets/[Scripts]/heart.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class heart : MonoBehaviour
 {
-
+    [SerializeField] GameObject heartFX;
     private PlayerLivesTracker livesTracker;    
     // Start is called before the first frame update
     void Start()
@@ -24,6 +24,8 @@ public class heart : MonoBehaviour
         {
             FindObjectOfType<Sound_Manager>().Play("heart");
             livesTracker.IncreaseLives(1);
+            GameObject obj = Instantiate(heartFX.gameObject, collision.transform);
+            collision.transform.parent = obj.transform;
             Destroy(gameObject);
         }
 
